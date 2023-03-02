2744911f8f0fb2c41503091f0585bee2b7224d20
Parvsub <Praveenboda421@gmail.com>
2023-03-02 12:45:11 +0530

    Added: Valid Email upadated to Verify invalid mails

--- ValidPincode.java
+++ ValidPincode.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidEmail {
-    public static void EmailValidation(String email){
+public class ValidPincode {
+    public static void Pincode(String code){
     String regex = ("[a-c01+.-]{3,7}+@[a-z.,1]{5,13}+");
     Pattern pattern = Pattern.compile(regex);
-    Matcher matcher = pattern.matcher(email);
+    Matcher matcher = pattern.matcher(code);
     boolean b = matcher.matches();
         System.out.println(b);
     }
@@ -17,6 +17,6 @@ public class ValidEmail {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the email");
         String email = scanner.next();
-        ValidEmail.EmailValidation(email);
+        ValidPincode.Pincode(email);
     }
 }
